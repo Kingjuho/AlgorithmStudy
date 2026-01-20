9d684eade197c2f72b76c55344f661aa3d4e15a6
Jhlee <ljh990627@gmail.com>
2026-01-20 21:10:34 +0900

    Create 2438.cs

---
+++ Baekjoon/2438/2438.cs
@@ -0,0 +1,8 @@
+﻿/*******
+ 2438. 별 찍기 - 1
+
+ * namespace, class 다 빼고 제출해도 정상작동
+ ********/
+
+int a = int.Parse(Console.ReadLine());
+for (int i = 1; i <= a; i++) Console.WriteLine(new string('*', i));
